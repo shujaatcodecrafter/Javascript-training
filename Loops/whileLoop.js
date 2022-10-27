3d3e0020fd09096881d7fb4da6c1ddcc6b6f502f
Rupesh Kumar <rupesh.kumar@taazaa.com>
2022-10-27 17:52:56 +0530

    [added whileLoop file]

---
+++ Loops/whileLoop.js
@@ -0,0 +1,26 @@
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+let i = 0;
+while (i < n) {
+    console.log(i)
+    i++;
+}
+
+
+const fruits = ["apple", "mango", "banana"]
+
+let i = 0;
+while (i < fruits.length) {
+    console.log(fruits[i])
+    i++;
+}
+
+
+
+const fruits1 = ["apple", "mango", "banana", "grapes"];
+
+let i = 0;
+while (i < fruits1.length) {
+    console.log(fruits1[i].toUpperCase());
+    i++;
+}
