dd696a6847b872a881ebb882cf3136f5066162c0
Rupesh Kumar <rupesh.kumar@taazaa.com>
2022-10-27 17:44:10 +0530

    Added forEach File

---
+++ Loops/forEach.js
@@ -0,0 +1,19 @@
+const numbers = [1, 2, 13, 4, 5, 8];
+
+const sum = 0;
+numbers.forEach(num => {
+    sum += num;
+})
+console.log
+
+
+
+const myFriends = ["Rupesh", "John", "vishal", "Virat"];
+
+myFriends.forEach(function (element, index, array) {
+    console.log(element);
+    console.log(index);
+    console.log(array);
+    console.log([element, index, array]);
+    console.log(element + " index : " + index);
+})
