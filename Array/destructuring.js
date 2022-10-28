4890e3d86bfd1127d759b8e72e08d00eaaf4b78f
Rupesh Kumar <rupesh.kumar@taazaa.com>
2022-10-28 16:35:45 +0530

    [Array destructuring , Object destructuring]

--- Array/destructuring.js
+++ Array/destructuring.js
@@ -1,6 +1,24 @@
+//Array destructuring 
 const myArray = ["value1", "value2", "value3", "value4"];
 let [myvar1, myvar2, ...myNewArray] = myArray;
 console.log("value of myvar1", myvar1)
 console.log("value of myvar2", myvar2);
 
 console.log(myNewArray);
+
+
+
+
+
+// Object Destructuring
+
+const car = {
+    brandName: "TATA",
+    year: 1947,
+    modal: "tata harrier",
+    country: "India"
+};
+
+let { brandName, year, ...restProps } = car;
+console.log(brandName);
+console.log(restProps);
