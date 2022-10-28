b20f64066ecdf5d75f27a465074abdd876cc8fa9
Rupesh Kumar <rupesh.kumar@taazaa.com>
2022-10-28 15:38:36 +0530

    [added spread operater file]

--- Array/spread.js
+++ Array/spread.js
@@ -8,3 +8,29 @@ array1.push("item3");
 
 console.log(array1 === array2);
 
+
+
+let arr1 = [0, 1, 2, 12];
+const arr2 = [3, 4, 5];
+
+arr1 = [...arr2, ...arr1];
+console.log(arr1)
+
+
+
+
+const myCar = {
+    brand: 'BMW',
+    model: 'B09',
+    color: 'Blue'
+}
+
+const mynewCar = {
+    type: 'car',
+    year: 2022,
+    color: 'yellow',
+}
+
+const myUpdatedCar = { ...myCar, ...mynewCar }
+console.log(myUpdatedCar);
+
