55e0bfcbc3783fdf217f5e895aa746e27a94e55f
Rupesh Kumar <rupesh.kumar@taazaa.com>
2022-10-28 17:59:46 +0530

    [added nested destructuring]

--- Array/destructuring.js
+++ Array/destructuring.js
@@ -9,7 +9,6 @@ console.log(myNewArray);
 
 
 
-
 // Object Destructuring
 
 const car = {
@@ -22,3 +21,18 @@ const car = {
 let { brandName, year, ...restProps } = car;
 console.log(brandName);
 console.log(restProps);
+
+
+
+//nested destructuring
+const users = [
+    { userId: 1, firstName: 'rupesh', gender: "male", city: "noida" },
+    { userId: 2, firstName: 'mohit', gender: "male", city: "delhi" },
+    { userId: 3, firstName: 'aakash', gender: "malesss", city: "pune" }
+]
+
+// const [{ firstName }, , { gender }] = users;
+const [{ firstName: user1firstName, userId }, , { gender: user3gender }] = users;
+console.log(user1firstName);
+console.log(user3gender);
+console.log(userId)
