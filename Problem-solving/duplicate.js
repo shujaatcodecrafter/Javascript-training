1a4672f3aa02e2e114be8254ac8c80484762efee
Rupesh Kumar <rupesh.kumar@taazaa.com>
2022-11-04 13:40:07 +0530

    Remove duplicate from array

--- Problem-solving/duplicate.js
+++ Problem-solving/duplicate.js
@@ -1,11 +1,36 @@
-let arr = [1, 1, 2, 4, 6, 7, 8, 1, 1, 2, 3, 4, 12, 15, 12, 13, 12, 90, 15, 15];
+// let arr = [1, 1, 2, 4, 6, 7, 8, 1, 1, 2, 3, 4, 12, 15, 12, 13, 12, 90, 15, 15];
 
-let newUniqueArr = [... new Set(arr)];
-console.log(newUniqueArr);
+// let newUniqueArr = [... new Set(arr)];
+// console.log(newUniqueArr);
 
 
 
-const item = ["orange", "mango", "apple", "mango", "pineapple", "apple"];
+const fruitList = ["orange", "mango", "apple", "mango", "pineapple", "apple"];
 
-let newUniqueItem = [... new Set(item)];
-console.log(newUniqueItem);
+// let newUniqueItem = [... new Set(fruitList)];
+// console.log(newUniqueItem);
+
+function removeDuplicate(fruitList) {
+    return fruitList.filter((fruits,
+        index) => fruitList.indexOf(fruits) === (index));
+}
+console.log(removeDuplicate(fruitList));
+
+
+
+//using filter method
+const numbers = [1, 1, 2, 4, 6, 7, 8, 1, 1, 2, 3, 4, 12, 15, 12, 13, 12, 90, 15, 15];
+
+const unique = numbers.filter((value, index) => {
+    // console.log(value, index);
+    return numbers.indexOf(value) === index;
+});
+console.log(unique);
+
+
+
+// function removeDuplicatNumbers(numbers) {
+//     return numbers.filter((num,
+//         index) => numbers.indexOf(num) === index);
+// }
+// console.log(removeDuplicatNumbers(numbers));
