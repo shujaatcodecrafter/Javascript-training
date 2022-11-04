26a004d3457f8c4b7c7a2444ae4d19523fb07657
Rupesh Kumar <rupesh.kumar@taazaa.com>
2022-11-04 17:48:45 +0530

    check given string is palindrome or not

--- Problem-solving/palindrome.js
+++ Problem-solving/palindrome.js
@@ -4,35 +4,49 @@
 // do not built in function 
 
 
-function checkPalindrome(string) {
-    //length of the string
-    const item = string.length;
-    console.log(item);
+// function checkPalindrome(string) {
+//length of the string
+// const len = string.length;
+// console.log(len);
 
 
-    //write logic here .
-    for (let i = 0; i < item; i++) {
-
-        // check first and last string are same 
-        if (string[i] === string[item - 1 - i]) {
-            return "yes it is a palindrome";
-        }
-        return 'not a palindrome'
-    }
-
-}
-
-
+//write logic here .
+// for (let i = 0; i < len / 2; i++) {
+// console.log(string[i]);
+// check if first and last string are same 
+// console.log(string[len - 1 - i]);
+// if (string[i] != string[len - 1 - i]) {
+// console.log("it is not a palindrome")
+// }
+// }
+// }
+// console.log("it is a palindrome");
 
 
 // take input
-const string = prompt('Enter a string: ');
+// const string = prompt('Enter a string: ');
 
 
 
 // call the function
-const value = checkPalindrome(string);
+// const value = checkPalindrome(string);
+
+
+// console.log(value);
 
 
 
-console.log(value);
+
+let string = prompt("Please enter a string");
+
+let len = string.length;
+let msg = "It is a Palindrome";
+for (let i = 0; i < len / 2; i++) {
+    // console.log(string[i])
+    // console.log(string[len - 1 - i]);
+    if (string[i] != string[len - 1 - i]) {
+        // check if first and last string are same 
+        msg = "it is not a palindrome"
+    }
+}
+console.log(`${string}: ${msg}`);
